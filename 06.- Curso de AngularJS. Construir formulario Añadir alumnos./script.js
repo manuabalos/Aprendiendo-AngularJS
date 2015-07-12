a2fd7f47b8135490322ee410b62429326d0e7c43
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-07-12 18:51:27 +0200

    06.- Curso de AngularJS. Construir formulario Añadir alumnos.

---
+++ 06.- Curso de AngularJS. Construir formulario Añadir alumnos./script.js
@@ -0,0 +1,14 @@
+angular.module('myApp',[]).controller('AlumnoController',function($scope){
+	$scope.alumnos=[
+		{nombre: "Manuel Ábalos", telefono: "11111", curso: "Sin curso"},
+		{nombre: "Juan Blanco", telefono: "22222", curso: "1º ESO"},
+		{nombre: "Alberto Herrera", telefono: "33333", curso: "2º ESO"},
+		{nombre: "Ana Mariño", telefono: "44444", curso: "2º ESO"},
+		{nombre: "Rosa Luxemburgo", telefono: "55555", curso: "3º ESO"}
+	];
+
+	$scope.Save = function(){
+		$scope.alumnos.push({ nombre: $scope.nuevoAlumno.nombre, telefono: $scope.nuevoAlumno.telefono, curso: $scope.nuevoAlumno.curso});
+	}
+
+});
