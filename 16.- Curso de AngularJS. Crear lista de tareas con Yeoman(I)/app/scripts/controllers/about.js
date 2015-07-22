77ba0604f51942f05b377f331763801918c53afd
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-07-22 19:10:20 +0200

    16.- Curso de AngularJS. Crear lista de tareas con Yeoman(I).

---
+++ 16.- Curso de AngularJS. Crear lista de tareas con Yeoman(I)/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 16CursoDeAngularJsCrearListaDeTareasConYeomaniApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the 16CursoDeAngularJsCrearListaDeTareasConYeomaniApp
+ */
+angular.module('16CursoDeAngularJsCrearListaDeTareasConYeomaniApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ 16.- Curso de AngularJS. Crear lista de tareas con Yeoman(I)/app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 16CursoDeAngularJsCrearListaDeTareasConYeomaniApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the 16CursoDeAngularJsCrearListaDeTareasConYeomaniApp
+ */
+angular.module('16CursoDeAngularJsCrearListaDeTareasConYeomaniApp')
+  .controller('MainCtrl', function ($scope) {
+  	$scope.tareas = ['Item 1','Item 2','Item 3','Item 4'];
+});
