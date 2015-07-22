30309661d7c86b4b4a2d9f500d2ffcd79ab9b322
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-07-22 18:51:44 +0200

    15.- Curso de AngularJS. Dependencias en una aplicación Yeoman.

---
+++ 15.- Curso de AngularJS. Dependencias en una aplicación Yeoman./app/scripts/controllers/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 13CursoDeAngularJsScaffoldingWebConYeomanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the 13CursoDeAngularJsScaffoldingWebConYeomanApp
+ */
+
+/*angular.module('13CursoDeAngularJsScaffoldingWebConYeomanApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'E2E',
+      'Protactor'
+    ];
+  });
+*/
+
+angular.module('13CursoDeAngularJsScaffoldingWebConYeomanApp').controller('MainCtrl',function($scope){
+	$scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'E2E',
+      'Protactor'
+    ];
+});
