a9b83f943450c921af030ad4cc554061984fde15
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-07-22 18:25:29 +0200

    14.- Curso de AngularJS. Estructura de una aplicación creada con Yeoman.

---
+++ 14.- Curso de AngularJS. Estructura de una aplicación creada con Yeoman./app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 13CursoDeAngularJsScaffoldingWebConYeomanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the 13CursoDeAngularJsScaffoldingWebConYeomanApp
+ */
+angular.module('13CursoDeAngularJsScaffoldingWebConYeomanApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
